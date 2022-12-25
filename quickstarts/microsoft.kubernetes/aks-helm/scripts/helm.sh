259860c30e252b57eae38e7d03befc1f3dc6663b
eliyahu77 <40737397+eliyahu77@users.noreply.github.com>
2022-12-25 13:06:47 +0200

    Update helm.sh

--- quickstarts/microsoft.kubernetes/aks-helm/scripts/helm.sh
+++ quickstarts/microsoft.kubernetes/aks-helm/scripts/helm.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Download and install Helm
-wget -O helm.tgz https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
+wget -O helm.tgz https://get.helm.sh/helm-v3.9.3-linux-amd64.tar.gz
 tar -zxvf helm.tgz
 mv linux-amd64/helm /usr/local/bin/helm
 # Install kubectl
@@ -10,6 +10,10 @@ az aks install-cli
 
 # Get cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTER_NAME
+
+# Create Namespace
+NAMESPACE=mlrun
+kubectl create ns $NAMESPACE
 
 # Install Simple Helm Chart https://github.com/bitnami/azure-marketplace-charts
 
@@ -23,6 +27,7 @@ helm search repo \
 helm install \
     $HELM_APP_NAME \
     $HELM_APP \
+    -n $NAMESPACE \
     --set global.imagePullSecrets={emptysecret}
 
 echo \{\"Status\":\"Complete\"\} > $AZ_SCRIPTS_OUTPUT_PATH
