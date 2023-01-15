aee38a238c9725261ccd3b272f50a06c02e625f7
eliyahun <eliyahun@iguazio.com>
2023-01-15 16:33:22 +0200

    update
    Signed-off-by: eliyahun <eliyahun@iguazio.com>

--- quickstarts/microsoft.kubernetes/aks-helm/scripts/helm.sh
+++ quickstarts/microsoft.kubernetes/aks-helm/scripts/helm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-sleep 80
+sleep 30
 # Download and install Helm
 wget -O helm.tgz https://get.helm.sh/helm-v3.9.3-linux-amd64.tar.gz
 tar -zxvf helm.tgz
 mv linux-amd64/helm /usr/local/bin/helm
 # Install kubectl
-az aks install-cli
+#az aks install-cli
 
 # Get cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTER_NAME
